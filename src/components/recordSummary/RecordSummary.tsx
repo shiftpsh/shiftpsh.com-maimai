b52665776e0f12cb8a1c325373c4b8d433480855
Suhyun Park <me@shiftpsh.com>
2024-07-30 18:20:02 +0900

    feat: chart type

--- src/components/recordSummary/RecordSummary.tsx
+++ src/components/recordSummary/RecordSummary.tsx
@@ -5,6 +5,10 @@ import { ellipsis } from "polished";
 import { difficultyBorderColor } from "../../utils/difficulty";
 import { wanpaku } from "../../styles/fonts/wanpaku";
 
+const MUSIC_DX_URL = "https://maimaidx-eng.com/maimai-mobile/img/music_dx.png";
+const MUSIC_STD_URL =
+  "https://maimaidx-eng.com/maimai-mobile/img/music_standard.png";
+
 const RecordContainer = styled.div`
   min-width: 0;
   border: 1px solid;
@@ -23,6 +27,15 @@ const Jacket = styled.img`
   display: block;
   width: 100%;
   aspect-ratio: 1 / 1;
+  user-select: none;
+`;
+
+const Type = styled.img`
+  position: absolute;
+  left: 0.5em;
+  top: 0.5em;
+  display: block;
+  height: 1em;
 `;
 
 const LevelMarkerContainer = styled.div`
@@ -64,7 +77,7 @@ interface Props {
 }
 
 const RecordSummary = ({ song }: Props) => {
-  const { jacketKey, difficulty, title, record } = song;
+  const { jacketKey, type, difficulty, title, record } = song;
   const { achievement, rank } = record;
 
   return (
@@ -77,6 +90,7 @@ const RecordSummary = ({ song }: Props) => {
         <Jacket
           src={`https://maimaidx-eng.com/maimai-mobile/img/Music/${jacketKey}.png`}
         />
+        <Type src={type === "DX" ? MUSIC_DX_URL : MUSIC_STD_URL} alt={type} />
       </JacketContainer>
       <LevelMarkerContainer>
         <LevelMarker song={song} />
